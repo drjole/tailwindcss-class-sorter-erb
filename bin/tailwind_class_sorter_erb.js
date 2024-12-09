7c66d5173fcc3e65679af8a8fdb4114bed9badda
Jonas Weber <jonas@jole.dev>
2024-12-09 13:47:22 +0100

    0.1.0

--- bin/tailwind_class_sorter_erb.js
+++ bin/tailwind_class_sorter_erb.js
@@ -1,5 +1,8 @@
-import { exit } from "process";
-import { readFileSync, writeFileSync } from "fs";
+#!/usr/bin/env node
+
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 import { format } from "prettier";
 
@@ -8,22 +11,30 @@ import EmbeddedTemplate from "tree-sitter-embedded-template";
 import HTML from "tree-sitter-html";
 import Ruby from "tree-sitter-ruby";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const embeddedParser = new Parser();
 embeddedParser.setLanguage(EmbeddedTemplate);
 const embeddedQuery = new Query(
     EmbeddedTemplate,
-    readFileSync("queries/embedded_template.scm")
+    readFileSync(join(__dirname, "../queries/embedded_template.scm")),
 );
 
 const htmlParser = new Parser();
 htmlParser.setLanguage(HTML);
-const htmlQuery = new Query(HTML, readFileSync("queries/html.scm"));
+const htmlQuery = new Query(
+    HTML,
+    readFileSync(join(__dirname, "../queries/html.scm")),
+);
 
 const rubyParser = new Parser();
 rubyParser.setLanguage(Ruby);
-const rubyQuery = new Query(Ruby, readFileSync("queries/ruby.scm"));
+const rubyQuery = new Query(
+    Ruby,
+    readFileSync(join(__dirname, "../queries/ruby.scm")),
+);
 
-async function formatSourceCode(sourceCode: string): Promise<string> {
+async function formatSourceCode(sourceCode) {
     const captures = [
         ...htmlCaptures(sourceCode).map((htmlCapture) => ({
             ...htmlCapture,
@@ -33,7 +44,7 @@ async function formatSourceCode(sourceCode: string): Promise<string> {
             rubyCaptures(embeddedCapture.node.text).map((rubyCapture) => ({
                 ...rubyCapture,
                 codeStartIndex: embeddedCapture.node.startIndex,
-            }))
+            })),
         ),
     ];
 
@@ -54,27 +65,33 @@ async function formatSourceCode(sourceCode: string): Promise<string> {
     return sourceCode;
 }
 
-function embeddedCaptures(sourceCode: string) {
+function embeddedCaptures(sourceCode) {
     const tree = embeddedParser.parse(sourceCode);
     return embeddedQuery.captures(tree.rootNode);
 }
 
-function htmlCaptures(sourceCode: string) {
+function htmlCaptures(sourceCode) {
     const tree = htmlParser.parse(sourceCode);
-    return htmlQuery.captures(tree.rootNode).filter((capture) => capture.name === "class_value");
+    return htmlQuery
+        .captures(tree.rootNode)
+        .filter((capture) => capture.name === "class_value");
 }
 
-function rubyCaptures(sourceCode: string) {
+function rubyCaptures(sourceCode) {
     const tree = rubyParser.parse(sourceCode);
-    return rubyQuery.captures(tree.rootNode).filter((capture) => capture.name === "class_value");
+    return rubyQuery
+        .captures(tree.rootNode)
+        .filter((capture) => capture.name === "class_value");
 }
 
-async function sortClasses(classes: string): Promise<string> {
+async function sortClasses(classes) {
     const formatted = await format(`<div class="${classes}"></div>`, {
         parser: "html",
         plugins: ["prettier-plugin-tailwindcss"],
     });
-    const formattedClasses = htmlCaptures(formatted).find((capture) => capture.name === "class_value");
+    const formattedClasses = htmlCaptures(formatted).find(
+        (capture) => capture.name === "class_value",
+    );
     if (!formattedClasses) {
         throw new Error("Could not find formatted classes");
     }
@@ -82,28 +99,9 @@ async function sortClasses(classes: string): Promise<string> {
 }
 
 async function main() {
-    const args = process.argv.slice(2);
-    const write = args.includes("--write");
-    const filePaths = args.filter((arg) => !arg.startsWith("--"));
-    if (filePaths.length === 0) {
-        console.error("No files specified");
-        exit(1);
-    }
-
-    if (!write && filePaths.length > 1) {
-        console.error("Cannot format multiple files without --write");
-        exit(1);
-    }
-
-    for (const filePath of filePaths) {
-        const sourceCode = readFileSync(filePath).toString();
-        const formattedCode = await formatSourceCode(sourceCode);
-        if (write) {
-            writeFileSync(filePath, formattedCode);
-        } else {
-            console.log(formattedCode);
-        }
-    }
+    const sourceCode = readFileSync(0, "utf-8").toString();
+    const formattedCode = await formatSourceCode(sourceCode);
+    process.stdout.write(formattedCode);
 }
 
 main();
