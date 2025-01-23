93d6fe410b54efde5d8675a49879c9ec13d164ce
Jonas Weber <jonas@jole.dev>
2025-01-23 16:09:05 +0100

    Add support for array of strings args for clases in ruby

--- queries/ruby.scm
+++ queries/ruby.scm
@@ -5,3 +5,13 @@
     (string_content) @class_value
   )
 )
+
+(pair
+  key: (hash_key_symbol) @key
+  (#eq? @key "class")
+  value: (array
+    (string
+      (string_content) @class_value
+    )
+  )
+)
