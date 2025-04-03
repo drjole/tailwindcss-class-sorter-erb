c1310359e91508c383baec3174413abae7a02847
Jonas Weber <jonas@jole.dev>
2025-04-03 16:27:34 +0200

    Fix sorting of long lines

--- tests/index.js
+++ tests/index.js
@@ -1,24 +1,29 @@
 import assert from "assert";
 import { test } from "node:test";
-import fs from "node:fs";
+import { promises as fs } from "node:fs";
 import { TailwindCSSClassSorterERB } from "../lib/tailwindcss_class_sorter_erb.js";
 
 const sorter = new TailwindCSSClassSorterERB();
 
-fs.readdir("tests/files", async function(_, files) {
-    // For each pair of files like "a.html.erb" and "a.expected.html.erb", run a test:
+async function loadTests() {
+    const files = await fs.readdir("tests/files");
+
     for (const file of files) {
         if (file.match(/^[a-z]\.html\.erb$/)) {
             const expectedFile = file.replace(/\.html\.erb$/, ".expected.html.erb");
 
-            const testContent = fs.readFileSync(`tests/files/${file}`, "utf8");
-            const expectedContent = fs.readFileSync(`tests/files/${expectedFile}`, "utf8");
+            // Define a test per file
+            test(`Test ${file}`, async () => {
+                const [testContent, expectedContent] = await Promise.all([
+                    fs.readFile(`tests/files/${file}`, "utf8"),
+                    fs.readFile(`tests/files/${expectedFile}`, "utf8"),
+                ]);
 
-            const testResult = await sorter.sort(testContent);
-
-            test(`Test ${file}`, () => {
+                const testResult = await sorter.sort(testContent);
                 assert.strictEqual(testResult, expectedContent);
             });
         }
     }
-});
+}
+
+await loadTests();
